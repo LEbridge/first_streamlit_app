bb91594e6fb42e5ea35524962068214b8ec898c0
LEbridge <135835927+LEbridge@users.noreply.github.com>
2023-06-09 18:31:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,4 +63,4 @@ if streamlit.button('Add a fruit to the list'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-streamlit.stop()
+#streamlit.stop()
